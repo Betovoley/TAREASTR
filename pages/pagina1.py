4e5e2954ebc4d13004879a9135b33923eec1f16a
Betovoley <beto_voley@hotmail.com>
2025-01-11 22:51:32 -0300

    listo para comenzar a realizar analisis

--- pages/pagina1.py
+++ pages/pagina1.py
@@ -1,6 +1,17 @@
 import streamlit as st
+from PIL import Image
+
+# Personalizando el nombre de lengueta
+img = Image.open('assets/bola.ico')
+st.set_page_config(page_title='Betovoley', page_icon=img, layout='wide', initial_sidebar_state='auto')
+
+# Cuerpo principal
+    # Agregar el logo
+st.image("assets/logo_sada.png", width=200)
+
 import common.login as login
 
 login.generarLogin()
 if 'usuario' in st.session_state:
-    st.header('Página 1')
+    st.header('Análise básica por equipe')
+
